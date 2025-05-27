5ab5f664d10049e66f88544fe2a56a0fcec6a532
Kumarjit Das <kumarjitdas43@gmail.com>
2025-05-28 00:09:15 +0530

    exception handler updated

--- src/core/middlewares/ExceptionHandler.ts
+++ src/core/middlewares/ExceptionHandler.ts
@@ -11,23 +11,27 @@ import { Response } from 'express';
 @Catch()
 export class ExceptionHandler implements ExceptionFilter {
   constructor() {}
-
-  catch(exception: unknown, host: ArgumentsHost): void {
-    // In certain situations `httpAdapter` might not be available in the
-    // constructor method, thus we should resolve it here.
-
+  catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    // const request = ctx.getRequest<Request>();
-    const httpStatus =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+    const request = ctx.getRequest<Request>();
 
-    const responseBody = {
-      status: httpStatus,
-      message: (exception as HttpException).message
-    };
-    response.status(httpStatus).json(responseBody)
+    let statusCode: number, error: any, message: any;
+    if (exception instanceof HttpException) {
+      statusCode = exception.getStatus();
+      error = exception.message;
+      message = exception.getResponse();
+    } else {
+      statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+      error = 'Internal server error';
+    }
+
+    response.status(statusCode).json({
+      statusCode,
+      error,
+      message: message && message.message ? message.message : error,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ExceptionHandler } from './core/middlewares/ExceptionHandler';
 import { ResponseHandler } from './core/middlewares/ResponseHandler';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+    bodyParser: false
+  });
   app.setGlobalPrefix("api")
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
-
+  app.use(helmet());
   // Auto-validation
-  // We'll start by binding ValidationPipe at the application level, thus ensuring all endpoints are protected from receiving incorrect data.
+  //We'll start by binding ValidationPipe at the application level, thus ensuring all endpoints are protected from receiving incorrect data.
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -21,6 +25,7 @@ async function bootstrap() {
     })
   );
   app.useGlobalInterceptors(new ResponseHandler())
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalFilters(new ExceptionHandler())
   await app.listen(process.env.PORT ?? 8080, () => {
     console.log('Running at http://localhost:' + (process.env.PORT?? 8080));
